ae5bef4c94c1bcbcd157c2cee13a4d7afdcb2fd6
wuxiei <2423858451@qq.com>
2022-03-21 23:48:29 +0800

    string+++

--- string+++/string+++.c
+++ string+++/string+++.c
@@ -69,21 +69,111 @@
 
 
 //结构体声明
-struct Stu
+//struct Stu
+//{
+//	char name[20];
+//	char tele[12];
+//	char sex[10];
+//	int age;
+//}s4,s5,s6;//全局变量
+//
+//struct Stu s3;//全局变量
+//
+//int main()
+//{
+//	struct Stu s1;//局部变量
+//	struct Stu s2;//局部变量
+//	struct Stu s7 = { "张三","1233445","男",14};
+//	printf("%s %s %s %d\n", s7.name, s7.tele, s7.sex, s7.age);
+//	return 0;
+//}
+
+//正确的自引用方法 - 类似于数据结构链表的查找
+//struct Node
+//{
+//	int data;
+//	struct Node* next;//指针的方法区=去自引用
+//};
+
+
+//struct T
+//{
+//	double w;
+//	int age;
+//};
+//struct S
+//{
+//	char c;
+//	struct T st;
+//	int a;
+//	double d;
+//	char arr[20];
+//};
+//int main()
+//{
+//	struct S s = { 'c',{3.14,30},6,7.7,"hello.." };
+//	printf("%c %If %d %d %If %s\n", s.c, s.st.w, s.st.age, s.a, s.d, s.arr);
+//	return 0;
+//}
+
+//结构体所占内存大小问题存在 - 结构体内存对齐
+//1.结构体第一个成员变量的地址就是结构体起始地址
+//2.其余成员变量；首先找对妻数 - 就是类型大小与默认对齐数中的较小值（vs中默认对齐数为8）然后找对齐数整数倍的地址放进去
+//3.整个结构体大小为最大对齐数的整数倍（整数倍就是看偏移量 - 第一个成员内存地址偏移量为0 第二个为1.....）
+
+//struct S1
+//{
+//	char c1;
+//	int a;
+//	char c2;
+//};
+//struct S2
+//{
+//	char c1;
+//	char c2;
+//	int a;
+//};
+//int main()
+//{
+//	struct S1 s1 = { 0 };
+//	printf("%d\n", sizeof(s1));//12
+//	struct S2 s2 = { 0 };
+//	printf("%d\n", sizeof(s2));//8
+//	return 0;
+//}
+
+//默认对齐数可以更改
+#pragma pack(4)//将默认对齐数改为4
+struct S
 {
-	char name[20];
-	char tele[12];
-	char sex[10];
-	int age;
-}s4,s5,s6;//全局变量
-
-struct Stu s3;//全局变量
-
+	char c;
+	int i;
+	double d;
+};
+void Init(struct S* ps)
+{
+	ps->c = 'w';
+	ps->i = 100;
+	ps->d = 3.14;
+}
+//传值调用 - 不可修改结构体内容且占用空间大
+void print1(struct S tmp)
+{
+	printf("%c %d %IF\n", tmp.c, tmp.i, tmp.d);
+}
+//传址调用 - 可以修改结构体内容且占用空间小
+//若不想结构体被修改可以写成：void print2(const struct S* tmp)
+void print2(struct S* tmp)
+{
+	tmp->c = 'h';
+	printf("%c %d %IF\n", tmp->c, tmp->i, tmp->d);
+}
 int main()
 {
-	struct Stu s1;//局部变量
-	struct Stu s2;//局部变量
-	struct Stu s7 = { "张三","1233445","男",14};
-	printf("%s %s %s %d\n", s7.name, s7.tele, s7.sex, s7.age);
+	struct S s = { 0 };
+	Init(&s);//初始化s
+	printf("%d\n", s.i);
+	print1(s);
+	print2(&s);//！！推荐传址！！
 	return 0;
 }
