3b03eec380775253cfa33e7df49d36d8a7fd2c51
wuxiei <2423858451@qq.com>
2022-04-04 23:27:24 +0800

    file

--- file/file/file.c
+++ file/file/file.c
@@ -5,8 +5,8 @@
 //文件操作
 
 //1.文件操作首先要打开文件  操作完后要关闭文件释放指针 这些步骤不能少
-int main()
-{
+//int main()
+//{
 
 	//写文件
 //	FILE* pf = fopen("text.txt", "w");//路径，打开方式
@@ -82,12 +82,117 @@ int main()
 
 
 	//从键盘上读取一行信息
-	char buf[1024] = { 0 };
-	/*fgets(buf, 1024, stdin);//从标准输入流读取
-	fputs(buf, stdout);//输出到标准输出流*/
-	//等价于下面的写法
-	gets(buf);
-	puts(buf);
+//	char buf[1024] = { 0 };
+//	/*fgets(buf, 1024, stdin);//从标准输入流读取
+//	fputs(buf, stdout);//输出到标准输出流*/
+//	//等价于下面的写法
+//	gets(buf);
+//	puts(buf);
+//
+//	return 0;
+//}
 
-	return 0;
-}
+
+	//struct S
+	//{
+	//	int n;
+	//	float score;
+	//	char arr[10];
+	//};
+
+	//int main()
+	//{
+	//	struct S s ={100,3.14f,"abcdef"};
+	//	struct S tmp = { 0 };
+	//	char buf[1024] = { 0 };
+	//	//把格式化的数据转换成字符串储存到buf
+	//	sprintf(buf, "%d %f %s", s.n, s.score, s.arr);
+	//	printf("%s\n", buf);
+	//	//从buf中读取格式化的数据到tmp中
+	//	sscanf(buf, "%d %f %s\n", &(tmp.n), &(tmp.score), tmp.arr);
+	//	printf("%d %f %s\n", tmp.n, tmp.score, tmp.arr);
+	//	return 0;
+	//}
+
+struct S
+{
+	char name[20];
+	int age;
+	double score;
+};
+//int main()
+//{
+//	struct S s = { "张三",20,55.6 };
+//	FILE* pf = fopen("text.tex", "wb");
+//	if (pf == NULL)
+//	{
+//		return;
+//	}
+//	//二进制形式写文件
+//	fwrite(&s, sizeof(struct S), 1, pf);//要写入的文件来自哪里，要写入的元素多大字节，几个元素，写入到哪里
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+//int main()
+//{
+//	struct S tmp = { 0 };
+//	FILE* pf = fopen("text.tex", "rb");
+//	if (pf == NULL)
+//	{
+//		return;
+//	}
+//	//二进制形式读取文件
+//	fread(&tmp, sizeof(struct S), 1, pf);//读出的数据放哪里，要读的元素多大字节，几个元素，从哪里读
+//	printf("%s %d %lf\n", tmp.name, tmp.age, tmp.score);
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+//	
+
+
+//int main()
+//{
+//	FILE* pf = fopen("text.txt", "r");
+//	if (pf == NULL)
+//	{
+//		return;
+//	}
+//	//1.定位文件指针
+//	fseek(pf, 4, SEEK_CUR);//SEEK_CUR文件指针的当前位置 向后偏移4个字节
+//	//2.读取文件
+//	int ch = fgetc(pf);
+//	printf("%c\n", ch);
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	FILE* pf = fopen("text.txt", "r");
+//	if (pf == NULL)
+//	{
+//		return;
+//	}
+//	//1.定位文件指针
+//	fseek(pf, -2, SEEK_END);//SEEK_END文件的末尾位置 向前偏移2个字节 注意这个偏移位置是负数！！！！
+//	//2.读取文件
+//	//int ch = fgetc(pf);
+//
+//	int pos = ftell(pf);//ftell(pf)获取文件读写指针的当前位置
+//	printf("%d\n", pos);
+//
+//	void rewind(pf);//rewind让文件指针位置回到文件起始位置
+//
+//	pos = ftell(pf);
+//	printf("%d\n", pos);
+//
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
